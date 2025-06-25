8d3f5a05ee569fc37e9d27be3f64a02c5504045e
Joao Victor Vieira <joaovconst@gmail.com>
2025-06-25 19:41:34 -0300

    inicio da interfaçe

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+//carrega a janela do app
 const { app, BrowserWindow } = require("electron");
 
 let mainWindow;
 
 app.whenReady().then(() => {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
+        //icone a baixo 
+        icon:__dirname + '/assets/calendar.png',
         webPreferences: {
             nodeIntegration: true
         }
     });
-
+    
+// Fechar o app corretamente quando o usuário sai
 mainWindow.loadFile("./pages/index.html");
     app.on("window-all-closed", () => {
         if (process.platform !== "darwin") {
