3989cbb48da5cd5ca32b81165b3995ceab427e53
Joao Victor Vieira <joaovconst@gmail.com>
2025-06-30 19:03:08 -0300

    Local storage e funções

---
+++ pages/script.js
@@ -0,0 +1,46 @@
+//local storage teste
+  let habits = JSON.parse(localStorage.getItem("habits")) || [];
+
+function renderHabits() {
+  const list = document.getElementById("habitList");
+  list.innerHTML = "";
+
+  habits.forEach((habit, index) => {
+    const li = document.createElement("li");
+    li.className = habit.done ? "habit-done" : "";
+    li.innerHTML = `
+    <span>${habit.name}</span>
+    <button onclick="toggleHabit(${index})">
+    ${habit.done ? "Desfazer" : "Feito"}
+    </button>
+    <button onclick="deleteHabit(${index})">🗑</button>
+`;
+
+    list.appendChild(li);
+  });
+}
+
+function addHabit() {
+  const input = document.getElementById("habitInput");
+  if (input.value.trim() === "") return;
+
+  habits.push({ name: input.value, done: false });
+  localStorage.setItem("habits", JSON.stringify(habits));
+  input.value = "";
+  renderHabits();
+}
+
+function toggleHabit(index) {
+  habits[index].done = !habits[index].done;
+  localStorage.setItem("habits", JSON.stringify(habits));
+  renderHabits();
+}
+
+function deleteHabit(index) {
+  habits.splice(index, 1);
+  localStorage.setItem("habits", JSON.stringify(habits));
+  renderHabits();
+}
+
+// Renderiza ao carregar a página
+renderHabits();
